f94a53ccb5cc3b773a5cea0c8cdb841d027ee6d1
yyehia <96464394+yyehia@users.noreply.github.com>
2021-12-20 23:12:39 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	
+	int n1=1;
+	int n2=1;
+	int temp;
+	cout<<n1;
+	cuot<<n2;
+	
+	
+	
+	
+	
+	
+	
+	/*string nome[4];
+	int voto1[4];
+	int voto2[4];
+	int media[4];
+	int somma[4];
+	int maggiore=0;
+	string nomem;
+	
+	for(int x = 0 ; x<4;x++)
+	{
+		cout<<"inserisci il "<<x+1<<"o nome"<<endl;
+		cin>>nome[x];
+		
+		cout<<"inserisci il primo voto dell' "<<x+1<<"o studente "<<endl;
+		cin>>voto1[x];
+		
+		cout<<"inserisci il secondo voto dell' "<<x+1<<"o studente "<<endl;
+		cin>>voto2[x];
+		
+		somma[x]= voto1[x]+voto2[x];
+		
+		media[x]=somma[x]/2;
+		
+		if(media[x]>maggiore)
+		{
+			maggiore=media[x];
+			nomem=nome[x];
+		}
+		
+		}
+		cout<<"la media piu' maggiore e' di "<<nomem<<" la sua media e' "<<maggiore<<endl;
+	    */
+	
+	
+	
+	return 0;
+}
